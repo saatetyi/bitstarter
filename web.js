d19984a21deb371f884eec3163d757cea89be127
saatetyi <szabo.peter@sch.bme.hu>
2013-07-09 00:29:23 +0000

    web.js v5

--- web.js
+++ web.js
@@ -3,8 +3,9 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var line = fs.readFileSync("index.html").toString('utf-8', 0, 27);
-var app.get('/', function(request, response){
+var line = fs.readFileSync("index.html").toString();
+
+app.get('/', function(request, response) {
  response.send(line);
 });
 
